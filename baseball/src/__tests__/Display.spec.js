7d68e900f8bbf7a41d2cb070d533ff4f53395585
steelworx <eileencuevas62@gmail.com>
2019-01-29 17:11:42 -0500

    added tests for strike button, foul button functionality

--- baseball/src/__tests__/Display.spec.js
+++ baseball/src/__tests__/Display.spec.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Display from '../components/Display';
 
 describe('Display Tests', () => {
-    // should display balls and strikes
-    // should have strikes incremented when strike, foul button hit
     // should have balls incremented when ball button hit
     // should reset when hit button hit
+
+    afterEach(cleanup);
+
     it('should display number of balls and strikes', () => {
         const { getByTestId } = render(<Display />);
 
@@ -17,5 +18,55 @@ describe('Display Tests', () => {
 
         expect(strikes).toHaveTextContent(0);
         expect(balls).toHaveTextContent(0);
+    })
+
+    it('should have strikes incremented when strike button is pressed', () => {
+        const { getByTestId } = render(<Display />);
+        const strikes = getByTestId('strike-count');
+        const strikeBtn = getByTestId('strike-btn');
+
+        fireEvent.click(strikeBtn);
+
+        expect(strikes).toHaveTextContent(1);
+
+    })
+
+    it('should reset when strike button is pressed 4 times', () => {
+        const { getByTestId } = render(<Display />);
+        const strikes = getByTestId('strike-count');
+        const strikeBtn = getByTestId('strike-btn');
+
+        fireEvent.click(strikeBtn);
+        fireEvent.click(strikeBtn);
+        fireEvent.click(strikeBtn);
+
+        expect(strikes).toHaveTextContent(0);
+
+    })
+
+    it('should have strikes incremented when foul button is pressed', () => {
+        const { getByTestId } = render(<Display />);
+
+        const strikes = getByTestId('strike-count');
+        const foulBtn = getByTestId('foul-btn');
+
+        fireEvent.click(foulBtn);
+
+        expect(strikes).toHaveTextContent(1);
+
+    })
+
+    it('should not have strikes incremented past 2 when foul button is pressed', () => {
+        const { getByTestId } = render(<Display />);
+
+        const strikes = getByTestId('strike-count');
+        const foulBtn = getByTestId('foul-btn');
+
+        fireEvent.click(foulBtn);
+        fireEvent.click(foulBtn);
+        fireEvent.click(foulBtn);
+
+        expect(strikes).toHaveTextContent(2);
+
     })
 })
