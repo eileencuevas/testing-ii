e6affc31ba498b81a5cff5671a93802d60ac6a9c
steelworx <eileencuevas62@gmail.com>
2019-01-29 16:57:06 -0500

    added functions for each button in order to change ball and strike count

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -3,10 +3,30 @@ import React from 'react';
 const Dashboard = props => {
     return(
         <div className='dashboard-container'>
-            <button>Strike</button>
-            <button>Ball</button>
-            <button>Foul</button>
-            <button>Hit</button>
+            <button 
+                onClick={props.wasStrike}
+                data-testid='strike-btn'
+            >
+                Strike
+            </button>
+            <button 
+                onClick={props.wasBall}
+                data-testid='ball-btn'
+            >
+                Ball
+            </button>
+            <button 
+                onClick={props.wasFoul}
+                data-testid='foul-btn'
+            >
+                Foul
+            </button>
+            <button 
+                onClick={props.wasHit}
+                data-testid='hit-btn'
+            >
+                Hit
+            </button>
         </div>
     );
 }
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -8,20 +8,55 @@ class Display extends React.Component {
         strikes: 0,
     }
 
+    wasStrike = () => {
+        if (this.state.strikes <= 2) {
+            this.setState({ strikes: this.state.strikes + 1 });
+        } else {
+            this.setState({ balls: 0, strikes: 0 });
+        }
+    }
+
+    wasBall = () => {
+        if (this.state.balls <= 3) {
+            this.setState({ balls: this.state.balls + 1 });
+        } else {
+            this.setState({ balls: 0, strikes: 0 });
+        }
+    }
+
+    wasFoul = () => {
+        if (this.state.strikes < 2) {
+            this.setState({ strikes: this.state.strikes + 1 });
+        }
+    }
+
+    wasHit = () => {
+        this.setState({ balls: 0, strikes: 0 });
+    }
+
     render() {
         return(
             <>
                 <div className='display-container'>
                     <div className='ball-container'>
                         <p>Balls:</p>
-                        <p>{this.state.balls}</p>
+                        <p data-testid='ball-count'>
+                            {this.state.balls}
+                        </p>
                     </div>
                     <div className='strike-container'>
                         <p>Strikes:</p>
-                        <p>{this.state.strikes}</p>
+                        <p data-testid='strike-count'>
+                            {this.state.strikes}
+                        </p>
                     </div>
                 </div>
-                <Dashboard />
+                <Dashboard 
+                    wasStrike={this.wasStrike}
+                    wasBall={this.wasBall}
+                    wasFoul={this.wasFoul}
+                    wasHit={this.wasHit}
+                />
             </>
         );
     }
