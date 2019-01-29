9783a6d22945ca72f36721137ca1d3ef523cf524
steelworx <eileencuevas62@gmail.com>
2019-01-29 18:27:27 -0500

    added ability to keep track of outs and innings

--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -6,13 +6,35 @@ class Display extends React.Component {
     state = {
         balls: 0,
         strikes: 0,
+        outs: 0,
+        inning: 1,
+        inningType: 'top',
     }
 
     wasStrike = () => {
         if (this.state.strikes < 2) {
             this.setState({ strikes: this.state.strikes + 1 });
         } else {
-            this.setState({ balls: 0, strikes: 0 });
+            if (this.state.outs === 2) {
+                if (this.state.inningType === 'top') {
+                    this.setState({ 
+                        balls: 0, 
+                        strikes: 0, 
+                        outs: 0, 
+                        inningType: 'bottom' 
+                    });
+                } else {
+                    this.setState({ 
+                        balls: 0, 
+                        strikes: 0, 
+                        outs: 0, 
+                        inning: this.state.inning + 1, 
+                        inningType: 'top' 
+                    });
+                }
+            } else {
+                this.setState({ balls: 0, strikes: 0, outs: this.state.outs + 1 });
+            }
         }
     }
 
@@ -48,6 +70,18 @@ class Display extends React.Component {
                         <p>Strikes:</p>
                         <p data-testid='strike-count'>
                             {this.state.strikes}
+                        </p>
+                    </div>
+                    <div className='out-container'>
+                        <p>Outs:</p>
+                        <p data-testid='out-count'>
+                            {this.state.outs}
+                        </p>
+                    </div>
+                    <div className='inning-container'>
+                        <p>Inning:</p>
+                        <p data-testid='inning-count'>
+                            {`${this.state.inningType} of the ${this.state.inning}`}
                         </p>
                     </div>
                 </div>
