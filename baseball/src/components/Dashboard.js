8ebf5c8d7863c7ed310fca9b064b052f81d1fa6f
steelworx <eileencuevas62@gmail.com>
2019-01-29 15:57:15 -0500

    created Dashboard functional component

---
+++ baseball/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = props => {
+    return(
+        <div className='dashboard-container'>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </div>
+    );
+}
+
+export default Dashboard;
